42746e88435d87628b3b9fbdb34ee2bdbb8a6b52
sadhasivam jayabalaganesan <sadha@shoprunner.com>
2019-10-09 10:53:37 -0400

    added osx.init

---
+++ osx.init.sh
@@ -0,0 +1,23 @@
+# BREW Installs required for OSX setup
+
+# Programming Languanges
+brew install node nvm
+brew install nvm
+brew install go
+brew install elixir
+brew install adoptopenjdk
+brew install gradle
+
+# Utilities 
+brew install zsh
+brew install jq
+brew install tree
+brew install tmux
+
+## cloud ops
+brew install kubernetes-cli
+brew install kubectl
+brew install awscli
+brew install aws-okta
+brew install terraform
+
