5150622a142cfec4df3be61bedba83919b2d6693
sadhasivam jayabalaganesan <sadha@shoprunner.com>
2020-02-25 23:58:11 -0500

    added install.sh

--- dotstrap.sh
+++ dotstrap.sh
@@ -3,6 +3,8 @@ function dotboot() {
     rm -rf ~/.vim/.vimrc    && ln symlink/vimrc ~/.vim/.vimrc   
     rm -rf ~/.gitconfig && ln symlink/gitconfig ~/.gitconfig
     rm -rf ~/.npmrc && ln symlink/npmrc ~/.npmrc
+
+    ./install/brew.sh
 }
 
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
--- install/brew.sh
+++ install/brew.sh
@@ -1,4 +1,8 @@
-# BREW Installs required for OSX setup
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap homebrew/versions
+brew tap homebrew/dupes
+brew update
+brew upgrade
 
 # Programming Languanges
 brew install node nvm
@@ -13,6 +17,7 @@ brew install zsh
 brew install jq
 brew install tree
 brew install tmux
+brew install wifi-password
 
 ## cloud ops
 brew install kubernetes-cli
@@ -22,6 +27,17 @@ brew install aws-okta
 brew install terraform
 
 # development
+brew install git
+brew install htop
+brew install nmap
+brew install wget
 brew install caddy
 # self certificate experiment
 brew install step 
+
+# Cask
+brew tap caskroom/cask
+brew install brew-cask
+brew tap caskroom/versions
+
+brew cask install --appdir="/Applications" 
